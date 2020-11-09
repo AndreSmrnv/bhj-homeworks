15efe554c4ec959abdacb1e1d0d1d57d8affc72e
AndreSm <tehmir@bk.ru>
2020-11-09 18:57:30 +0300

    1- function closeSubMenu() { bhj-homeworks rem

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -13,3 +13,15 @@ for (let i = 0, len = linksMenuMain.length; i < len; i++) {
     linksMenuMain[i].onclick = openSubMenu;
 
 }
+function closeSubMenu() {
+    const elementsMenuSub = this.querySelector('ul.menu_sub');
+    if (elementsMenuSub) {
+        elementsMenuSub.className = 'menu menu_sub';
+
+    }
+}
+const elementsMenuMain = document.querySelectorAll('ul.menu_main > li');
+for (let i = 0, len = elementsMenuMain.length; i < len; i++) {
+    elementsMenuMain[i].onmouseleave = closeSubMenu;
+
+}
