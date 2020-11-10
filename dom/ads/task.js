fcf8ea9feae6cca0ac5f9261ee3d0b36cf99687d
AndreSm <tehmir@bk.ru>
2020-11-10 10:40:29 +0300

    dom\ads bhj-homeworks

---
+++ dom/ads/task.js
@@ -0,0 +1,16 @@
+let timer = setInterval(changeRotator, 1000);
+const elementsRotatorCase = document.querySelectorAll('.rotator > span.rotator__case');
+let lengthRotatorCase = elementsRotatorCase.length;
+function changeRotator() {
+    for (let i = 0; i < lengthRotatorCase; i++) {
+        if (elementsRotatorCase[i].classList.contains('rotator__case_active')) {
+            elementsRotatorCase[i].classList.toggle('rotator__case_active');
+            if (i === (lengthRotatorCase - 1)) {
+                elementsRotatorCase[0].classList.toggle('rotator__case_active');
+            } else {
+                elementsRotatorCase[++i].classList.toggle('rotator__case_active');
+            }
+        }
+    }
+    //console.log(lengthRotatorCase);
+}
