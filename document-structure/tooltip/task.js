d7192fb03169f3c61bb35a4a7c76853ffef8c048
AndreSm <tehmir@bk.ru>
2020-11-13 12:41:23 +0300

    document-structure\tooltip bhj-homeworks ver2

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -7,10 +7,24 @@ for (const elementHasTooltip of elementsHasTooltip) {
 
 function showTooltip(event) {
     event.preventDefault();
-    const elementDivTooltip = document.createElement('div');
-    elementDivTooltip.classList.add('tooltip', 'tooltip_active');
-    elementDivTooltip.innerText = this.title;
-    this.insertAdjacentElement('afterEnd', elementDivTooltip);
+    hideTooltips();
+    const elementNext = this.nextElementSibling;
+    if (Boolean(elementNext) && elementNext.classList.contains('tooltip')) {
+        elementNext.classList.toggle('tooltip_active');
+    } else {
+        const elementDivTooltip = document.createElement('div');
+        elementDivTooltip.classList.add('tooltip', 'tooltip_active');
+        elementDivTooltip.innerText = this.title;
+        elementDivTooltip.dataset.position = 'right';
+        this.insertAdjacentElement('afterEnd', elementDivTooltip);
+    }
 
+
+}
+function hideTooltips() {
+    const elementsActiveTooltip = document.querySelectorAll('.tooltip_active');
+    for (const elementActiveTooltip of elementsActiveTooltip) {
+        elementActiveTooltip.classList.toggle('tooltip_active');;
+    }
 }
 
