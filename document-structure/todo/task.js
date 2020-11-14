bf1159a43719f1745cb7aa1b5f031dde8aa81f29
AndreSm <tehmir@bk.ru>
2020-11-14 09:44:54 +0300

    document-structure\todo bhj-homeworks ver3

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,34 +4,32 @@ tasksForm.addEventListener('submit', event => { event.preventDefault(); });
 const elementTaskList = document.getElementById('tasks__list');
 
 const elementTaskInput = document.getElementById('task__input');
-elementTaskInput.addEventListener('keyup', addTask);
-function addTask(event) {
-    if ((event.code == 'Enter' || event.code == 'NumpadEnter') && (elementTaskInput.value.length)) {
-
-
-        elementTaskList.innerHTML += `
+elementTaskInput.addEventListener('keyup', checkEnter);
+const elementBtnTaskAdd = document.getElementById('tasks__add');
+elementBtnTaskAdd.addEventListener('click', addTask);
+function checkEnter(event) {
+    if ((event.key === 'Enter')) {
+        addTask();
+    }
+}
+function addTask() {
+    if (elementTaskInput.value.length) {
+        elementTaskList.insertAdjacentHTML('beforeEnd', `
             <div class="task">
                 <div class="task__title">
                     ${elementTaskInput.value}
                 </div>
                 <a href="#" class="task__remove">&times;</a>
             </div>
-        `;
+        `);
         const elementTaskRemove = elementTaskList.lastElementChild.querySelector('.task__remove');
         elementTaskRemove.addEventListener('click', delTask);
-        /*
-            почему у меня событие остается только на последней задаче? я же добавляю последовательно на каждую создаваемую задачу. 
-            предыдущие события по какой причине пропадают?
-        */
-        //console.log(`elementTaskList.lastChild - ${elementTaskList.lastElementChild.querySelector('.task__title').textContent}`);
         tasksForm.reset();
     }
 }
 
 function delTask(event) {
     event.preventDefault();
-    //console.log(`taskRemove - ${this.parentNode.querySelector('.task__title').textContent}`);
     this.parentNode.remove();
-
 }
 
